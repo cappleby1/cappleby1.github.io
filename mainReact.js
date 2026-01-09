950dbddd3a5300fee2afafc118f278aaee0cb0c6
Cat Appleby <catappleby1@proton.me>
2026-01-09 12:15:31 +0000

    Footer on splash & updates to project descriptions

--- mainReact.js
+++ mainReact.js
@@ -34,7 +34,7 @@ const projects = [
     id: "undergrad-react",
     title: "Undergraduate Dissertation",
     shortDesc: "This project was to create a more interactive way for the user to complete physiotherapy. The project was created in unity and used OpenCV with python sockets.",
-    longDesc: "By using only a webcam the user could use their hand to move an in-game hand that could interact with the game world such as activating buttons. Things such as the players time would be recorded so that they were able to track improvements on time. With more time more features such as accuracy would have been added.",
+    longDesc: "By using only a webcam the user could use their hand to move an in-game hand that could interact with the game world. This would include things such as activating buttons. The game would record the player's time so that they were able to track progress for their rehabilitation. With more time more features such as accuracy tracking would have been added or different ways of interacting with objects in the world. This would could be increased scope wise to a near full game project.",
     githubLink: "https://github.com/cappleby1/undergraduate_dissertation/tree/main"
   },
   {
